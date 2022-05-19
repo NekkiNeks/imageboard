4a40817b323de3de944976be6863e145cd8eb1a4
NekkiNeks <nekkineks@cocine.ninja>
2022-05-19 18:35:03 +0300

    added post request handling for adding posts

--- backend/index.js
+++ backend/index.js
@@ -86,43 +86,52 @@ async function getComments(id) {
   return comments;
 }
 
-async function addPost(thread_id, answer_to, title, content, file) {
+async function addPost(title, content, file) {
+  const query = {
+    text: "INSERT INTO posts (time, title, content, image) VALUES ($1, $2, $3, $4)",
+    values: ["NOW()", title, content, file ? file.path : null],
+  };
+  client.query(query, (err, res) => {
+    if (err) {
+      throw new Error(err.stack);
+    } else {
+      return "post was created";
+    }
+  });
+}
+
+async function addComment(thread_id, answer_to, title, content, file) {
   let answers = answer_to.length > 0 ? answer_to.split(",") : "";
-  if (!content) {
-    throw new Error("please enter content");
-  }
-  //SQL queries
+  //SQL querie
   const query = {
     text: "INSERT INTO comments (time, title, content, image, thread_id) VALUES ($1, $2, $3, $4, $5) RETURNING id",
     values: ["NOW()", title, content, file ? file.path : null, thread_id],
   };
-  let comment_id;
+
   //query for comment
   client.query(query, (err, res) => {
     if (err) {
       console.log(err.stack);
-      return err.stack;
+      throw new Error(err.stack);
     } else {
-      console.log("responce from SQL:");
-      console.log(res.rows);
-      comment_id = res.rows[0].id;
-    }
-    //loop for comment answers
-    for (let answer_to of answers) {
-      const answerQuery = {
-        text: "INSERT INTO commentsrelations (comment_id, answer_to) VALUES ($1, $2)",
-        values: [comment_id, answer_to],
-      };
-      client.query(answerQuery, (err, res) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(`answer to ${answer_to} added!`);
-        }
-      });
+      //loop for comment answers
+      const comment_id = res.rows[0].id;
+      for (let answer_to of answers) {
+        const answerQuery = {
+          text: "INSERT INTO commentsrelations (comment_id, answer_to) VALUES ($1, $2)",
+          values: [comment_id, answer_to],
+        };
+        client.query(answerQuery, (err, res) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(`answer to ${answer_to} added!`);
+          }
+        });
+      }
     }
   });
-  return `post with id ${comment_id} was posted`;
+  return `post was posted`;
 }
 
 app.get("/posts/", (req, res) => {
@@ -151,14 +160,36 @@ app.get("/comments/:id", (req, res) => {
     );
 });
 
+app.post("/posts", upload.single("file"), (req, res) => {
+  const { title, content } = req.body;
+  const file = req.file ? req.file : null;
+
+  if (!title & !content) {
+    res.status(200).send({
+      status: "failed",
+      message: "text and title field cant be empty",
+    });
+  }
+
+  addPost(title, content, file)
+    .then((responce) => res.send({ status: "success", data: responce }))
+    .catch((err) =>
+      res.status(500).send({ status: "failed", message: err.message })
+    );
+});
+
 app.post("/comments/", upload.single("file"), (req, res) => {
   const { thread_id, title, content, answer_to } = req.body;
   const file = req.file ? req.file : null;
 
-  addPost(thread_id, answer_to, title, content, file)
+  if (!content) {
+    res.status(200).send({ status: "failed", message: "Text cant be empty" });
+  }
+
+  addComment(thread_id, answer_to, title, content, file)
     .then((responce) => res.send({ status: "success", data: responce }))
     .catch((err) =>
-      res.status(200).send({ status: "failed", message: err.message })
+      res.status(500).send({ status: "failed", message: err.message })
     );
 });
 
